18369d9cc886cfc7c1c8f81197fdb73c9254e0f7
Vaibhav Gupta <vaigupta4@publicisgroupe.net>
2021-10-11 22:06:21 +0100

    remove unused code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET default route. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "iTunes API works" });
 });
--- views/task.js
+++ views/task.js
@@ -4,51 +4,54 @@ const inviteUser = async (req, res) => {
   const authUrl = "https://url.to.auth.system.com/invitation";
 
   try {
-    const resp = await superagent.post(authUrl);
-    resp.send(invitationBody).end((err, invitationResponse) => {
-      const { status, body } = invitationResponse;
-      const { invitationId, authId } = body;
+    const invitationResponse = await superagent
+      .post(authUrl)
+      .send(invitationBody);
 
-      const query = { authId: invitationResponse.body.authId };
-      const update = {
-        authId,
-        email: invitationBody.email,
-      };
-      const options = {
-        upsert: true,
-        new: true,
-      };
+    const { status, body } = invitationResponse;
+    const { invitationId, authId } = body;
 
-      const callback = (errInUser, createdUser) => {
-        Shop.findById(shopId).exec((errInShop, shop) => {
-          if (errInShop || !shop) {
-            return res
-              .status(500)
-              .send(errInShop || { message: "No shop found" });
-          }
-          if (shop.invitations.includes(invitationId)) {
-            shop.invitations.push(invitationId);
-          }
-          if (!shop.users.includes(createdUser._id)) {
-            shop.users.push(createdUser);
-          }
-          shop.save();
-        });
-      };
+    const query = { authId: invitationResponse.body.authId };
+    const update = {
+      authId,
+      email: invitationBody.email,
+    };
+    const options = {
+      upsert: true,
+      new: true,
+    };
 
-      if (status === 201) {
-        User.findOneAndUpdate(query, update, options, callback);
-      } else if (status === 200) {
-        res.status(400).json({
-          error: true,
-          message: "User already invited to this shop",
-        });
-        return;
-      }
-      res.json(invitationResponse);
-    });
-  } catch (e) {
-    console.log(e);
+    const callback = (errInUser, createdUser) => {
+      // request must contains shopId
+      Shop.findById(shopId).exec((errInShop, shop) => {
+        if (errInShop || !shop) {
+          return res
+            .status(400) // It should return status 400 (not 500) if shopId not found.
+            .send(errInShop || { message: "No shop found" });
+        }
+        // Add users in shop db if not already present
+        if (!shop.invitations.includes(invitationId)) {
+          shop.invitations.push(invitationId);
+        }
+        if (!shop.users.includes(createdUser._id)) {
+          shop.users.push(createdUser);
+        }
+        shop.save();
+      });
+    };
+
+    if (status === 201) {
+      User.findOneAndUpdate(query, update, options, callback);
+    } else if (status === 200) {
+      res.status(400).json({
+        error: true,
+        message: "User already invited to this shop",
+      });
+      return;
+    }
+    res.json(invitationResponse);
+  } catch (err) {
+    console.log(err);
   }
 };
 
