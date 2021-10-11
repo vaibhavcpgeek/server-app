4181e84169153c5f1b7a466bad3c958116d2b248
Vaibhav Gupta <vaigupta4@publicisgroupe.net>
2021-10-11 23:42:33 +0100

    refactored

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 var cors = require("cors");
 
-const indexRouter = require("./routes/index");
-const artistRouter = require("./routes/artists");
+const router = require("./routes/index");
 
 const app = express();
 
@@ -23,8 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/artists/", artistRouter);
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require("express");
 var router = express.Router();
+const albumService = require("../services/album");
+const unique = require("./../filters/unique");
 
 /* GET default route. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "iTunes API works" });
+});
+
+/* GET artists route. */
+router.get("/artists", function (req, res, next) {
+  // To be Implemented
+  res.render("index", { title: "Artists route works" });
+});
+
+/* GET individual artist route. */
+router.get("/artists/:id", function (req, res, next) {
+  // To be Implemented
+  res.render("index", { title: "Individual Artist route works" });
+});
+
+/* GET default route. */
+router.get("/artists/:id/albums", async (req, res, next) => {
+  const artistId = req.params.id;
+  try {
+    const resp = await albumService.albums(artistId);
+    const uniqueCollection = unique(resp.results.slice(1));
+    res.status(200).json(uniqueCollection);
+  } catch (err) {
+    res.status(500).json(err, {
+      message: "Something went wrong",
+    });
+  }
 });
 
 module.exports = router;
